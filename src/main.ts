dfc6fe4f90b063c2c22ce3e8519c30e09f233288
tkgstrator <nasawake.am@gmail.com>
2022-12-22 18:14:20 +0900

    update: bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder, OpenAPIObject } from '@nestjs/swagger';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import * as path from 'path';
 import { mkdir, writeFileSync } from 'fs';
@@ -8,6 +8,18 @@ import { exec } from 'child_process';
 import * as bodyParser from 'body-parser';
 import { config } from 'dotenv';
 config({ path: '.env.sandbox' });
+
+async function build(documents: OpenAPIObject) {
+  const build = path.resolve(process.cwd(), 'docs');
+  const output = path.resolve(build, 'index');
+  mkdir(build, { recursive: true }, (_) => {});
+  writeFileSync(`${output}.json`, JSON.stringify(documents), {
+    encoding: 'utf8',
+  });
+  const dump = require('js-yaml').dump;
+  writeFileSync(`${output}.yaml`, dump(documents, {}));
+  exec(`npx redoc-cli build ${output}.json -o ${output}.html`);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -23,30 +35,21 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: '1',
   });
-  if (process.env.NODE_ENV === 'production') {
-    app.useGlobalPipes(
-      new ValidationPipe({ disableErrorMessages: true, transform: true })
-    );
-  } else {
-    app.useGlobalPipes(
-      new ValidationPipe({ disableErrorMessages: false, transform: true })
-    );
-  }
+  const disableErrorMessages: boolean = process.env.NODE_ENV === 'production';
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: disableErrorMessages,
+      transform: true,
+    })
+  );
   const options = new DocumentBuilder()
     .setTitle('Salmon Stats NEXT WAVE API Documents')
     .setVersion(process.env.API_VER)
     .build();
   const documents = SwaggerModule.createDocument(app, options);
+  SwaggerModule.createDocument;
   if (process.env.NODE_ENV !== 'production') {
-    const build = path.resolve(process.cwd(), 'docs');
-    const output = path.resolve(build, 'index');
-    mkdir(build, { recursive: true }, (_) => {});
-    writeFileSync(`${output}.json`, JSON.stringify(documents), {
-      encoding: 'utf8',
-    });
-    const dump = require('js-yaml').dump;
-    writeFileSync(`${output}.yaml`, dump(documents, {}));
-    exec(`npx redoc-cli build ${output}.json -o ${output}.html`);
+    build(documents);
   }
   SwaggerModule.setup('documents', app, documents);
   await app.listen(process.env.PORT || 3000);
