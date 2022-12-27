60a7d022d67b0080e7e37a1008058c148d4b97a8
tkgstrator <nasawake.am@gmail.com>
2022-12-28 01:11:56 +0900

    add: schedules

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -3,11 +3,14 @@ import { PrismaService } from 'src/prisma.service';
 import { ApiController } from './api.controller';
 import { ApiService } from './api.service';
 import { ResultsModule } from './results/results.module';
+import { SchedulesModule } from './schedules/schedules.module';
 import { ResultsService } from './results/results.service';
+import { SchedulesService } from './schedules/schedules.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   controllers: [ApiController],
-  providers: [PrismaService, ApiService, ResultsService],
-  imports: [ResultsModule, ResultsModule],
+  providers: [PrismaService, ApiService, ResultsService, SchedulesService],
+  imports: [ResultsModule, SchedulesModule, HttpModule],
 })
 export class ApiModule {}
---
+++ src/api/schedules/schedules.module.ts
@@ -0,0 +1,12 @@
+import { HttpModule } from '@nestjs/axios';
+import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { SchedulesController } from './schedules.controller';
+import { SchedulesService } from './schedules.service';
+
+@Module({
+  imports: [HttpModule],
+  controllers: [SchedulesController],
+  providers: [SchedulesService, PrismaService],
+})
+export class SchedulesModule {}
