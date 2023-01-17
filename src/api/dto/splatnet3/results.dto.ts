5ff5cd7a00d27eda1df0518ed41fc1e8781de84c
tkgstrator <nasawake.am@gmail.com>
2023-01-18 08:24:48 +0900

    fix: offset, limit parameters

--- src/api/dto/splatnet3/results.dto.ts
+++ src/api/dto/splatnet3/results.dto.ts
@@ -1,12 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
-import { ArrayMaxSize, IsArray, ValidateNested } from "class-validator";
+import { ArrayMaxSize, ArrayMinSize, arrayMinSize, IsArray, ValidateNested } from "class-validator";
 
 import { CoopResultRequest, CustomCoopResultRequest } from "./result.dto";
 
 export class ResultRequest {
   @ApiProperty({ type: [CoopResultRequest] })
   @IsArray()
+  @ArrayMinSize(1)
   @ArrayMaxSize(50)
   @ValidateNested({ each: true })
   @Type(() => CoopResultRequest)
@@ -16,6 +17,7 @@ export class ResultRequest {
 export class CustomResultRequest {
   @ApiProperty({ type: [CustomCoopResultRequest] })
   @IsArray()
+  @ArrayMinSize(1)
   @ArrayMaxSize(200)
   @ValidateNested({ each: true })
   @Type(() => CustomCoopResultRequest)
