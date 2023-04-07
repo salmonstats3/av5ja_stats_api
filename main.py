9baa8632ac90a1aed4c03b52fa1b15152e97d028
tkgstrator <nasawake.am@gmail.com>
2023-04-08 02:51:11 +0900

    update: request controller

--- main.py
+++ main.py
@@ -5,15 +5,16 @@ import json
 def upload():
   files = sorted(list(map(lambda x: int(x.split(".")[0]), os.listdir("results"))))
   for file in files:
-    with open(f"results/{file}.json", mode="r") as f:
-      request = json.loads(f.read())
-      print(f"Uploading {file}.json")
-      headers = {"Content-Type": "application/json"}
-      response = requests.post("http://localhost:8080/v2/results", data=json.dumps(request), headers=headers)
-      if response.status_code != 201:
-        with open(f"status.log", mode="a") as w:
-          w.write(f"{response.status_code}: {file}")
-          print(response.text)
+    if file >= 834000:
+      with open(f"results/{file}.json", mode="r") as f:
+        request = json.loads(f.read())
+        print(f"Uploading {file}.json")
+        headers = {"Content-Type": "application/json"}
+        response = requests.post("http://localhost:8080/v2/results", data=json.dumps(request), headers=headers)
+        if response.status_code != 201:
+          with open(f"status.log", mode="a") as w:
+            w.write(f"{response.status_code}: {file}")
+            print(response.text)
 
 def download():
   for offset in range(757000, 780000, 1000):
