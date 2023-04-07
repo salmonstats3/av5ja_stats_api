94cc340da49e6e02bd04f70e4c2c39bb95eea616
tkgstrator <nasawake.am@gmail.com>
2023-04-07 10:32:03 +0900

    update: restore method

--- src/api/results/results.controller.ts
+++ src/api/results/results.controller.ts
@@ -3,6 +3,7 @@ import { ApiBadRequestResponse, ApiExtraModels, ApiOperation, ApiTags } from "@n
 import { Result } from "@prisma/client";
 
 import { PaginatedDto } from "../dto/pagination.dto";
+import { CustomCoopResultManyRequest } from "../dto/request.custom.dto";
 import { CoopResultManyRequest } from "../dto/results/results.request.dto";
 
 import { ResultsService } from "./results.service";
@@ -22,7 +23,21 @@ export class ResultsController {
     operationId: "登録(SplatNet3)",
   })
   @ApiBadRequestResponse()
-  createMany(@Body() request: CoopResultManyRequest): Promise<Result[]> {
+  upsertMany(@Body() request: CoopResultManyRequest): Promise<Result[]> {
     return this.service.upsertMany(request);
+  }
+
+  @Post("")
+  @Version("2")
+  @HttpCode(201)
+  @ApiTags("リザルト")
+  @ApiOperation({
+    description:
+      "Salmonia3+形式のデータを最大同時に200件まで登録します。`results`のキーを指定して、JSONデータを配列で送信してください。",
+    operationId: "登録(SplatNet3)",
+  })
+  @ApiBadRequestResponse()
+  createMany(@Body() request: CustomCoopResultManyRequest): Promise<Result[]> {
+    return this.service.createMany(request);
   }
 }
