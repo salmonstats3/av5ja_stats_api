73fe4778e1f1a017eb1c47b65a8d05c8da0f2145
tkgstrator <nasawake.am@gmail.com>
2023-04-11 14:50:44 +0900

    update: use cache in 1 hour

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,19 +1,19 @@
 import { HttpModule } from "@nestjs/axios";
-import { Module } from "@nestjs/common";
+import { CacheModule, Module } from "@nestjs/common";
 import { PrismaService } from "src/prisma.service";
 
 import { ApiController } from "./api.controller";
 import { ApiService } from "./api.service";
+import { AuthorizeController } from "./authorize/authorize.controller";
+import { AuthorizeModule } from "./authorize/authorize.module";
 import { ResultsModule } from "./results/results.module";
 import { ResultsService } from "./results/results.service";
 import { SchedulesModule } from "./schedules/schedules.module";
 import { SchedulesService } from "./schedules/schedules.service";
-import { AuthorizeController } from "./authorize/authorize.controller";
-import { AuthorizeModule } from "./authorize/authorize.module";
 
 @Module({
   controllers: [ApiController, AuthorizeController],
-  imports: [ResultsModule, SchedulesModule, HttpModule, AuthorizeModule],
+  imports: [ResultsModule, SchedulesModule, HttpModule, AuthorizeModule, CacheModule.register()],
   providers: [PrismaService, ApiService, ResultsService, SchedulesService],
 })
 export class ApiModule {}
--- src/api/schedules/schedules.module.ts
+++ src/api/schedules/schedules.module.ts
@@ -1,5 +1,5 @@
 import { HttpModule } from "@nestjs/axios";
-import { Module } from "@nestjs/common";
+import { CacheModule, Module } from "@nestjs/common";
 import { PrismaService } from "src/prisma.service";
 
 import { SchedulesController } from "./schedules.controller";
@@ -7,7 +7,7 @@ import { SchedulesService } from "./schedules.service";
 
 @Module({
   controllers: [SchedulesController],
-  imports: [HttpModule],
+  imports: [HttpModule, CacheModule.register({ ttl: 60 * 60 * 6 })],
   providers: [SchedulesService, PrismaService],
 })
 export class SchedulesModule {}
