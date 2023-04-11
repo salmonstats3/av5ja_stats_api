b94150235aa0256f240b2531561369bd8f2fe2bf
tkgstrator <nasawake.am@gmail.com>
2023-04-11 14:50:44 +0900

    update: use cache in 1 hour

--- src/api/schedules/schedules.service.ts
+++ src/api/schedules/schedules.service.ts
@@ -1,5 +1,6 @@
 import { HttpService } from "@nestjs/axios";
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { CACHE_MANAGER, Inject, Injectable, NotFoundException } from "@nestjs/common";
+import { Cache } from "cache-manager";
 import { plainToClass } from "class-transformer";
 import dayjs from "dayjs";
 import { initializeApp } from "firebase/app";
@@ -19,18 +20,30 @@ import { firebaseConfig } from "../firebase.config";
 
 @Injectable()
 export class SchedulesService {
-  constructor(private readonly axios: HttpService, private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly axios: HttpService,
+    private readonly prisma: PrismaService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
+
   readonly app = initializeApp(firebaseConfig);
   readonly firestore = getFirestore(this.app);
 
   // スケジュール一覧を返す
   async get_schedules(): Promise<CoopScheduleDataResponse[]> {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const values: any[] = await this.cacheManager.get("schedules");
+    if (values !== undefined) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      return values.map((value: any) => plainToClass(CoopScheduleDataResponse, value));
+    }
     const documents = await Promise.all(
       Object.values(Setting).map((setting) => getDocs(collection(this.firestore, setting))),
     );
     const schedules: CoopScheduleDataResponse[] = documents.flatMap((document) =>
       document.docs.map((doc) => plainToClass(CoopScheduleDataResponse, doc.data())),
     );
+    this.cacheManager.set("schedules", schedules, { ttl: 60 * 60 * 1 });
     return schedules.sort((a, b) => dayjs(a.startTime).unix() - dayjs(b.startTime).unix());
   }
 
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
-import { CacheInterceptor, CacheModule, Module } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { APP_INTERCEPTOR } from "@nestjs/core";
 
 import { ApiModule } from "./api/api.module";
 import { AppController } from "./app.controller";
@@ -10,21 +9,10 @@ import { AppService } from "./app.service";
   controllers: [AppController],
   imports: [
     ApiModule,
-    CacheModule.registerAsync({
-      useFactory: () => ({
-        ttl: 10,
-      }),
-    }),
     ConfigModule.forRoot({
       envFilePath: [".env.prod"],
     }),
   ],
-  providers: [
-    AppService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CacheInterceptor,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
