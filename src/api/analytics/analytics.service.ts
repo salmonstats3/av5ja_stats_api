5354cf5f8bd35339fe82a5dae981f1cedef8e196
tkgstrator <nasawake.am@gmail.com>
2023-04-24 00:32:03 +0900

    add: credentials in environment

--- src/api/analytics/analytics.service.ts
+++ src/api/analytics/analytics.service.ts
@@ -18,6 +18,7 @@ export class AnalyticsService {
   ) {}
 
   async getAnalytics(): Promise<AnalyticsResponseDto> {
+    console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
     const analytics: AnalyticsResponseDto = await this.cacheManager.get("analytics");
     if (analytics !== undefined) {
       return analytics;
