8a7b6428d6d58366bffc0cf8362374ed4ff45093
tkgstrator <nasawake.am@gmail.com>
2023-04-25 09:45:09 +0900

    fix: version api endpoint

--- src/api/authorize/authorize.controller.ts
+++ src/api/authorize/authorize.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, HttpCode, Version } from "@nestjs/common";
 import { ApiBadRequestResponse, ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 
+import { AppVersionResponse } from "../dto/authorize/app_version.dto";
+
 import { AuthorizeService } from "./authorize.service";
 import { AuthorizeResponse } from "./autorize.response.dto";
 
@@ -20,5 +22,19 @@ export class AuthorizeController {
   @ApiOkResponse({ type: AuthorizeResponse })
   async authorize(): Promise<AuthorizeResponse> {
     return this.service.authorize();
+  }
+
+  @Get("version")
+  @Version("1")
+  @HttpCode(200)
+  @ApiTags("スケジュール")
+  @ApiOperation({
+    description: "イカリング3のスケジュールを取得して書き込みます",
+    operationId: "スケジュール書き込み",
+  })
+  @ApiBadRequestResponse()
+  @ApiOkResponse({ type: AppVersionResponse })
+  async version(): Promise<AppVersionResponse> {
+    return this.service.get_app_version();
   }
 }
