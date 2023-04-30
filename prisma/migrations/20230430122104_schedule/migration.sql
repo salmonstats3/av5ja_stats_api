60d68dfe8ac9cf55308512281913c47daaf269e6
tkgstrator <nasawake.am@gmail.com>
2023-04-30 22:08:33 +0900

    fix: rename

--- prisma/migrations/20230430122104_schedule/migration.sql
+++ prisma/migrations/20230430122104_schedule/migration.sql
@@ -3,6 +3,8 @@
 
   - A unique constraint covering the columns `[id,schedule_id]` on the table `results` will be added. If there are existing duplicate values, this will fail.
   - Added the required column `scheduleId` to the `players` table without a default value. This is not possible if the table is not empty.
+  - Made the column `start_time` on table `schedules` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `end_time` on table `schedules` required. This step will fail if there are existing NULL values in that column.
 
 */
 -- DropForeignKey
@@ -10,6 +12,12 @@ ALTER TABLE "players" DROP CONSTRAINT "players_id_fkey";
 
 -- AlterTable
 ALTER TABLE "players" ADD COLUMN     "scheduleId" TEXT NOT NULL;
+
+-- AlterTable
+ALTER TABLE "schedules" ALTER COLUMN "start_time" SET NOT NULL,
+ALTER COLUMN "start_time" SET DEFAULT '1970-01-01 00:00:00 +00:00',
+ALTER COLUMN "end_time" SET NOT NULL,
+ALTER COLUMN "end_time" SET DEFAULT '1970-01-01 00:00:00 +00:00';
 
 -- CreateIndex
 CREATE UNIQUE INDEX "results_id_schedule_id_key" ON "results"("id", "schedule_id");
