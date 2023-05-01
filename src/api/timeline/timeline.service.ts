810610aa14abc5dec758592672ffb6978d6709a2
tkgstrator <nasawake.am@gmail.com>
2023-05-01 10:36:56 +0900

    add: analytics

--- src/api/timeline/timeline.service.ts
+++ src/api/timeline/timeline.service.ts
@@ -4,8 +4,6 @@ import { CACHE_MANAGER, Inject, Injectable } from "@nestjs/common";
 import { Cache } from "cache-manager";
 import { PrismaService } from "src/prisma.service";
 
-
-
 @Injectable()
 export class AnalyticsService {
   analyticsDataClient: BetaAnalyticsDataClient = new BetaAnalyticsDataClient();
@@ -15,11 +13,82 @@ export class AnalyticsService {
     private readonly axios: HttpService,
     private readonly prisma: PrismaService,
     @Inject(CACHE_MANAGER) private readonly cacheManager: Cache,
-  ) { }
-  
+  ) {}
+
+  async getGradePoint(scheduleId: string): Promise<any> {
+    return this.prisma.$queryRaw`
+      WITH players AS (
+        SELECT
+        npln_user_id,
+        MAX(grade_point) AS grade_point
+        FROM
+        players
+        WHERE
+        schedule_id = ${scheduleId}
+        AND
+        grade_point IS NOT NULL
+        GROUP BY
+        npln_user_id
+      )
+      SELECT
+      (CASE grade_point WHEN 999 THEN 999 ELSE grade_point / 10 * 10 END) AS grade_point,
+      COUNT(*)::INT
+      FROM
+      players
+      GROUP BY
+      (CASE grade_point WHEN 999 THEN 999 ELSE grade_point / 10 * 10 END)
+      ORDER BY
+      grade_point
+    `;
+  }
+
+  async getIkuraNum(scheduleId: string): Promise<any> {
+    return this.prisma.$queryRaw`
+      WITH results AS (
+        SELECT
+        npln_user_id,
+        MAX(results.golden_ikura_num) AS golden_ikura_num,
+        MAX(results.ikura_num) AS ikura_num
+        FROM
+        results
+        INNER JOIN
+        players
+        ON
+        players.id = results.id
+        WHERE
+        results.schedule_id = ${scheduleId}
+        GROUP BY
+        npln_user_id
+    )
+    SELECT
+    golden_ikura_num / 5 * 5 AS golden_ikura_num,
+    COUNT(*)::INT
+    FROM
+    results
+    GROUP BY
+    golden_ikura_num / 5 * 5
+    ORDER BY
+    golden_ikura_num
+    `;
+  }
+
+  async getAnalytics(scheduleId: string): Promise<any> {
+    const analytics = await this.cacheManager.get(`analytics:${scheduleId}`);
+    if (analytics !== undefined) {
+      return analytics;
+    }
+    const results: any[] = await Promise.all([this.getGradePoint(scheduleId), this.getIkuraNum(scheduleId), this.getTimeline(scheduleId)]);
+    const response = {
+      golden_ikura_num: results[1],
+      grade_point: results[0],
+      status: results[2],
+    };
+    this.cacheManager.set(`analytics:${scheduleId}`, response, { ttl: 60 * 60 * 1 });
+    return response;
+  }
+
   async getTimeline(scheduleId: string): Promise<any> {
-    return this.prisma.$queryRaw
-      `
+    return this.prisma.$queryRaw`
       SELECT
       DATE_TRUNC('HOUR', play_time) + CAST(EXTRACT(MINUTE FROM play_time)::INT / 30 * 30 || ' MINUTES' AS INTERVAL) AS play_time,
       AVG(danger_rate * 100 * 5 - 800)::DECIMAL(6, 3) AS grade_point,
@@ -47,6 +116,6 @@ export class AnalyticsService {
       GROUP BY
       DATE_TRUNC('HOUR', play_time) + CAST(EXTRACT(MINUTE FROM play_time)::INT / 30 * 30 || ' MINUTES' AS INTERVAL)
       ORDER BY play_time DESC
-      `
+      `;
   }
 }
