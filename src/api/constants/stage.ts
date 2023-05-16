c016fb2334080bb91d91645774616ffb0a4966ac
tkgstrator <nasawake.am@gmail.com>
2023-05-16 11:45:05 +0900

    add golden random weapon

--- src/api/constants/stage.ts
+++ src/api/constants/stage.ts
@@ -1,4 +1,4 @@
-export const stageLists: { [name: string]: number } = {
+const StageType = {
   "0b0376955f3909e9e95b81c640c66094f8f5d6b316d4fdf89f865dd332d6a13a": 5,
   "1a29476c1ab5fdbc813e2df99cd290ce56dfe29755b97f671a7250e5f77f4961": 6,
   "2276a46e42a11637776ebc15cf2d46a589f1dba34a76d5c940c418ed7371d071": 100,
@@ -11,4 +11,12 @@ export const stageLists: { [name: string]: number } = {
   be584c7c7f547b8cbac318617f646680541f88071bc71db73cd461eb3ea6326e: 1,
   f1e4df4cff1dc5e0acc66a9654fecf949224f7e4f6bd36305d4600ac3fa3db7b: 7,
   ffa84f05a6437395a0a128cad1a99e8dd0f303ce4fd687fa648617a0075d7ad9: -1,
-};
+} as const;
+
+type StageType = (typeof StageType)[keyof typeof StageType];
+
+export namespace StageKey {
+  export function from(key: string): StageType {
+    return StageType[key];
+  }
+}
