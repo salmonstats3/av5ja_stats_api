4ef424ac5d0baf313e9996312dff64efd85d3df8
tkgstrator <nasawake.am@gmail.com>
2023-09-12 09:57:51 +0900

    Update SQL

--- prisma/migrations/20230912005745_/migration.sql
+++ prisma/migrations/20230912005745_/migration.sql
@@ -3,6 +3,8 @@
 
   - The primary key for the `players` table will be changed. If it partially fails, the table could be left without primary key constraint.
   - The primary key for the `results` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `updated_by` on the `results` table. All the data in the column will be lost.
+  - You are about to drop the column `version` on the `results` table. All the data in the column will be lost.
 
 */
 -- DropForeignKey
@@ -20,13 +22,24 @@ DROP INDEX "players_npln_user_id_play_time_id_key";
 -- DropIndex
 DROP INDEX "results_id_schedule_id_play_time_key";
 
+-- DropIndex
+DROP INDEX "schedules_schedule_id_key";
+
 -- AlterTable
 ALTER TABLE "players" DROP CONSTRAINT "players_pkey",
 ADD CONSTRAINT "players_pkey" PRIMARY KEY ("npln_user_id", "play_time", "id");
 
 -- AlterTable
 ALTER TABLE "results" DROP CONSTRAINT "results_pkey",
+DROP COLUMN "updated_by",
+DROP COLUMN "version",
 ADD CONSTRAINT "results_pkey" PRIMARY KEY ("id", "schedule_id", "play_time");
+
+-- DropEnum
+DROP TYPE "Client";
+
+-- CreateIndex
+CREATE INDEX "schedules_schedule_id_idx" ON "schedules"("schedule_id");
 
 -- AddForeignKey
 ALTER TABLE "results" ADD CONSTRAINT "results_schedule_id_fkey" FOREIGN KEY ("schedule_id") REFERENCES "schedules"("schedule_id") ON DELETE CASCADE ON UPDATE CASCADE;
