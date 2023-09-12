3b8275c8e4a653c598b2912cfb817e9ce3d7732e
tkgstrator <nasawake.am@gmail.com>
2023-09-12 10:14:25 +0900

    Initialize

--- src/api/dto/enum/water_level.ts
+++
@@ -1,8 +0,0 @@
-/**
- * サーモンランの潮位
- */
-export enum WaterLevel {
-  LOW_TIDE = 0,
-  NORMAL = 1,
-  HIGH_TIDE = 2,
-}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,11 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { Module } from "@nestjs/common";
 
-import { ApiModule } from './api/api.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
   controllers: [AppController],
-  imports: [
-    ApiModule,
-    ConfigModule.forRoot({
-      envFilePath: ['.env'],
-    }),
-  ],
+  imports: [],
   providers: [AppService],
 })
 export class AppModule {}
