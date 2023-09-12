ec6bfc183426e1814f0d764e3bf15fe5a3a618df
tkgstrator <nasawake.am@gmail.com>
2023-09-12 16:10:20 +0900

    Add filter, hook

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,12 @@ import * as path from "path";
 
 import { LogLevel, VersioningType, ValidationPipe } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { NestFactory } from "@nestjs/core";
+import { HttpAdapterHost, NestFactory } from "@nestjs/core";
 import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
 import { DocumentBuilder, SwaggerModule, OpenAPIObject } from "@nestjs/swagger";
 import fastify from "fastify";
 import { dump } from "js-yaml";
+import { PrismaClientExceptionFilter } from "nestjs-prisma";
 
 import { AppModule } from "./app.module";
 import { onSend, preValidation } from "./fastify/log";
@@ -29,16 +30,26 @@ async function bootstrap() {
   const isDevelopment = process.env.NODE_ENV === "development";
 
   const server = fastify();
+  // ログレベル
   const logLevels: LogLevel[] = isDevelopment ? ["log", "error", "warn", "debug", "verbose"] : ["log", "error", "warn"];
+  // ログ出力
   server.addHook("preValidation", preValidation);
   server.addHook("onSend", onSend);
+  server.addHook("onRequest", (request, reply, done) => {
+    const replyUnknown = reply as any;
+    replyUnknown["setHeader"] = reply.header.bind(reply);
+    replyUnknown["end"] = reply.send.bind(reply);
+    done();
+  });
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   const adapter = new FastifyAdapter(server);
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, adapter, {
     logger: logLevels,
   });
+  // 設定ファイル読み込み
   const config = app.get(ConfigService);
+  // バージョンとCORSの設定
   app.enableVersioning({ defaultVersion: "1", type: VersioningType.URI });
   app.enableCors({
     allowedHeaders:
@@ -49,6 +60,10 @@ async function bootstrap() {
     origin: "*",
     preflightContinue: false,
   });
+  // フィルターの追加(意味はよくわかっていない)
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+  // バリデーション時に変換する
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -60,6 +75,7 @@ async function bootstrap() {
     }),
   );
 
+  // 環境変数を読み込んで値がなければエラーを返す
   const version = config.get<string>("API_VERSION");
   const port = config.get<number>("API_PORT");
   const host = config.get<string>("API_HOST");
@@ -73,6 +89,7 @@ async function bootstrap() {
   if (version === undefined) {
     throw new Error("API_VERSION is not defined");
   }
+  // 開発環境時はSwaggerを有効にする
   if (isDevelopment) {
     const documentConfig = new DocumentBuilder()
       .setTitle("Salmon Stats+")
@@ -85,6 +102,7 @@ async function bootstrap() {
     SwaggerModule.setup("docs", app, document);
   }
 
+  // アプリを起動
   await app.listen(port, host);
 }
 bootstrap();
