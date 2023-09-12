49bc72e6ba8667187572046586ea20a4d37e30f8
tkgstrator <nasawake.am@gmail.com>
2023-09-12 15:40:16 +0900

    Update Fastify docs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from "@nestjs/common";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { ConfigModule } from '@nestjs/config'
+import { PrismaModule } from 'nestjs-prisma';
 
 @Module({
   controllers: [AppController],
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.local', '.env.production'],
+    }),
+    PrismaModule.forRoot({ isGlobal: true }),
+  ],
   providers: [AppService],
 })
 export class AppModule {}
