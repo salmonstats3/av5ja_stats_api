27e11cd5586b92382155cb3669f97fa57db548f0
tkgstrator <nasawake.am@gmail.com>
2023-09-12 12:49:25 +0900

    Use Fastify

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
+import fastifyCors from "@fastify/cors";
+import fastify from "fastify";
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
+import { LogLevel } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const server = fastify();
+  server.register(fastifyCors);
+    server.addHook('onRequest', (request, reply, done) => {
+    const replyUnknown = reply as any;
+    replyUnknown['setHeader'] = reply.header.bind(reply);
+    replyUnknown['end'] = reply.send.bind(reply);
+    done();
+  });
+  const adapter = new FastifyAdapter();
+  const logLevels: LogLevel[] = ["log", "error", "warn"];
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, adapter, {
+    logger: logLevels,
+  });
 }
 bootstrap();
