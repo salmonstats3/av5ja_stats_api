08a1538736f7407b7419168b36098960bde63112
tkgstrator <nasawake.am@gmail.com>
2023-09-18 13:01:33 +0900

    Add dummy endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,24 @@ import { PrismaModule } from "nestjs-prisma";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { SchedulesController } from "./schedules/schedules.controller";
+import { SchedulesModule } from "./schedules/schedules.module";
+import { SchedulesService } from "./schedules/schedules.service";
+import { ResultsController } from './results/results.controller';
+import { ResultsService } from './results/results.service';
+import { ResultsModule } from './results/results.module';
 
 @Module({
-  controllers: [AppController],
+  controllers: [AppController, SchedulesController, ResultsController],
   imports: [
     ConfigModule.forRoot({
       envFilePath: [".env", ".env.local", ".env.production"],
       isGlobal: true,
     }),
     PrismaModule.forRoot({ isGlobal: true }),
+    SchedulesModule,
+    ResultsModule,
   ],
-  providers: [AppService],
+  providers: [AppService, SchedulesService, ResultsService],
 })
 export class AppModule {}
