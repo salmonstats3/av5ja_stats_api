89b9d7dc9427f7b1b5924917394beb51ffb81736
tkgstrator <nasawake.am@gmail.com>
2023-10-20 13:26:16 +0900

    Add migration files

--- prisma/migrations/20230509010438_/migration.sql
+++ prisma/migrations/20230509010438_/migration.sql
@@ -7,15 +7,20 @@ CREATE TYPE "Mode" AS ENUM ('REGULAR', 'LIMITED', 'PRIVATE_CUSTOM', 'PRIVATE_SCE
 -- CreateEnum
 CREATE TYPE "Species" AS ENUM ('INKLING', 'OCTOLING');
 
+-- CreateEnum
+CREATE TYPE "Client" AS ENUM ('SALMONIA', 'SALMDROID');
+
 -- CreateTable
 CREATE TABLE "schedules" (
-    "schedule_id" VARCHAR(32) NOT NULL,
-    "start_time" TIMESTAMP(3),
-    "end_time" TIMESTAMP(3),
+    "schedule_id" UUID NOT NULL,
+    "start_time" TIMESTAMP(0) NOT NULL DEFAULT '1970-01-01 00:00:00 +00:00',
+    "end_time" TIMESTAMP(0) NOT NULL DEFAULT '1970-01-01 00:00:00 +00:00',
     "stage_id" SMALLINT NOT NULL,
     "weapon_list" INTEGER[],
     "mode" "Mode" NOT NULL DEFAULT 'REGULAR',
     "rule" "Rule" NOT NULL DEFAULT 'REGULAR',
+    "rare_weapon" SMALLINT,
+    "boss_id" SMALLINT,
     "created_at" TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(0) NOT NULL,
 
@@ -24,10 +29,9 @@ CREATE TABLE "schedules" (
 
 -- CreateTable
 CREATE TABLE "results" (
-    "result_id" VARCHAR(32) NOT NULL,
-    "uuid" UUID NOT NULL,
-    "schedule_id" VARCHAR(32) NOT NULL,
-    "play_time" TIMESTAMP(3) NOT NULL,
+    "id" UUID NOT NULL,
+    "schedule_id" UUID NOT NULL,
+    "play_time" TIMESTAMP(0) NOT NULL,
     "boss_counts" SMALLINT[],
     "boss_kill_counts" SMALLINT[],
     "ikura_num" SMALLINT NOT NULL,
@@ -46,12 +50,17 @@ CREATE TABLE "results" (
     "scenario_code" VARCHAR(16),
     "created_at" TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(0) NOT NULL,
+    "updated_by" "Client" NOT NULL DEFAULT 'SALMONIA',
+    "version" VARCHAR(16) NOT NULL,
 
-    CONSTRAINT "results_pkey" PRIMARY KEY ("play_time","uuid")
+    CONSTRAINT "results_pkey" PRIMARY KEY ("id","play_time")
 );
 
 -- CreateTable
 CREATE TABLE "players" (
+    "id" UUID NOT NULL,
+    "schedule_id" UUID NOT NULL,
+    "play_time" TIMESTAMP(0) NOT NULL,
     "npln_user_id" VARCHAR(20) NOT NULL,
     "name" VARCHAR(32) NOT NULL,
     "byname" VARCHAR(64) NOT NULL,
@@ -80,14 +89,15 @@ CREATE TABLE "players" (
     "weapon_list" SMALLINT[],
     "created_at" TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(0) NOT NULL,
-    "play_time" TIMESTAMP(3) NOT NULL,
-    "uuid" UUID NOT NULL,
 
-    CONSTRAINT "players_pkey" PRIMARY KEY ("play_time","uuid","npln_user_id")
+    CONSTRAINT "players_pkey" PRIMARY KEY ("npln_user_id","play_time")
 );
 
 -- CreateTable
 CREATE TABLE "waves" (
+    "id" UUID NOT NULL,
+    "schedule_id" UUID NOT NULL,
+    "play_time" TIMESTAMP(0) NOT NULL,
     "wave_id" SMALLINT NOT NULL,
     "water_level" SMALLINT NOT NULL,
     "event_type" SMALLINT NOT NULL,
@@ -97,10 +107,8 @@ CREATE TABLE "waves" (
     "is_clear" BOOLEAN NOT NULL,
     "created_at" TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(0) NOT NULL,
-    "play_time" TIMESTAMP(3) NOT NULL,
-    "uuid" UUID NOT NULL,
 
-    CONSTRAINT "waves_pkey" PRIMARY KEY ("play_time","uuid","wave_id")
+    CONSTRAINT "waves_pkey" PRIMARY KEY ("id","wave_id","play_time")
 );
 
 -- CreateIndex
@@ -116,10 +124,10 @@ CREATE INDEX "schedules_mode_idx" ON "schedules"("mode");
 CREATE INDEX "schedules_rule_mode_idx" ON "schedules"("rule", "mode");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "schedules_start_time_end_time_stage_id_mode_rule_weapon_lis_key" ON "schedules"("start_time", "end_time", "stage_id", "mode", "rule", "weapon_list");
+CREATE UNIQUE INDEX "schedules_stage_id_mode_rule_weapon_list_start_time_end_tim_key" ON "schedules"("stage_id", "mode", "rule", "weapon_list", "start_time", "end_time");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "results_result_id_key" ON "results"("result_id");
+CREATE UNIQUE INDEX "schedules_schedule_id_key" ON "schedules"("schedule_id");
 
 -- CreateIndex
 CREATE INDEX "results_members_idx" ON "results"("members");
@@ -137,13 +145,25 @@ CREATE INDEX "results_scenario_code_idx" ON "results"("scenario_code");
 CREATE INDEX "results_danger_rate_idx" ON "results"("danger_rate");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "results_id_schedule_id_play_time_key" ON "results"("id", "schedule_id", "play_time");
+
+-- CreateIndex
 CREATE INDEX "players_npln_user_id_idx" ON "players"("npln_user_id");
+
+-- CreateIndex
+CREATE INDEX "players_schedule_id_idx" ON "players"("schedule_id");
 
 -- CreateIndex
 CREATE INDEX "players_name_idx" ON "players"("name");
 
 -- CreateIndex
 CREATE INDEX "players_grade_point_idx" ON "players"("grade_point");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "players_npln_user_id_play_time_id_key" ON "players"("npln_user_id", "play_time", "id");
+
+-- CreateIndex
+CREATE INDEX "waves_schedule_id_idx" ON "waves"("schedule_id");
 
 -- CreateIndex
 CREATE INDEX "waves_water_level_idx" ON "waves"("water_level");
@@ -158,10 +178,10 @@ CREATE INDEX "waves_water_level_event_type_idx" ON "waves"("water_level", "event
 CREATE INDEX "waves_golden_ikura_num_idx" ON "waves"("golden_ikura_num");
 
 -- AddForeignKey
-ALTER TABLE "results" ADD CONSTRAINT "results_schedule_id_fkey" FOREIGN KEY ("schedule_id") REFERENCES "schedules"("schedule_id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "results" ADD CONSTRAINT "results_schedule_id_fkey" FOREIGN KEY ("schedule_id") REFERENCES "schedules"("schedule_id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "players" ADD CONSTRAINT "players_play_time_uuid_fkey" FOREIGN KEY ("play_time", "uuid") REFERENCES "results"("play_time", "uuid") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "players" ADD CONSTRAINT "players_id_schedule_id_play_time_fkey" FOREIGN KEY ("id", "schedule_id", "play_time") REFERENCES "results"("id", "schedule_id", "play_time") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "waves" ADD CONSTRAINT "waves_play_time_uuid_fkey" FOREIGN KEY ("play_time", "uuid") REFERENCES "results"("play_time", "uuid") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "waves" ADD CONSTRAINT "waves_id_schedule_id_play_time_fkey" FOREIGN KEY ("id", "schedule_id", "play_time") REFERENCES "results"("id", "schedule_id", "play_time") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ prisma/migrations/20231019000000_/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE results RENAME COLUMN id TO uuid;
+
+-- AlterTable
+ALTER TABLE waves RENAME COLUMN id TO uuid;
+
+-- AlterTable
+ALTER TABLE players RENAME COLUMN id TO uuid;
