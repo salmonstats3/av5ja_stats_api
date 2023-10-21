8abe335e526f620690cbb418ee30eee4c7dfe7c6
tkgstrator <nasawake.am@gmail.com>
2023-10-22 04:03:46 +0900

    Update SQL

--- prisma/migrations/20231019000000_/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE results RENAME COLUMN id TO uuid;
-
--- AlterTable
-ALTER TABLE waves RENAME COLUMN id TO uuid;
-
--- AlterTable
-ALTER TABLE players RENAME COLUMN id TO uuid;
