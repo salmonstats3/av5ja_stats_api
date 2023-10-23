622b2d9d9e4a37bb3596e774afe2850c8d36652d
tkgstrator <nasawake.am@gmail.com>
2023-10-24 01:30:18 +0900

    Define types

--- src/dto/version.dto.ts
+++ src/dto/version.dto.ts
@@ -3,7 +3,10 @@ import { Expose, Type } from 'class-transformer';
 import { ValidateNested } from 'class-validator';
 
 export class Version {
+  @ApiProperty({ default: '2.7.0' })
   readonly version: string;
+
+  @ApiProperty({ default: '4.0.0-dae4328c' })
   readonly web_version: string;
 }
 
