2f6f475bbc9dca66e958c50522cdc4c43f5f083d
tkgstrator <nasawake.am@gmail.com>
2023-12-13 02:01:24 +0900

    Schedules (#21)
    * update schedules
    
    * update version to 4.0.3
    
    * fix structure

--- src/schedules/schedules.service.ts
+++ src/schedules/schedules.service.ts
@@ -16,9 +16,9 @@ export class SchedulesService {
 
   private readonly firestore = getFirestore(initializeApp(firebaseConfig));
 
-  async create(request: StageScheduleQuery.Request): Promise<CoopHistoryQuery.Schedule[]> {
+  async create(request: StageScheduleQuery.Request): Promise<CoopHistoryQuery.Schedules> {
     await this.prisma.schedule.createMany(request.create);
-    return request.schedules.map((schedule) => plainToInstance(CoopHistoryQuery.Schedule, schedule, { excludeExtraneousValues: true }));
+    return plainToInstance(CoopHistoryQuery.Schedules, { schedules: request.schedules }, { excludeExtraneousValues: true });
   }
 
   /**
