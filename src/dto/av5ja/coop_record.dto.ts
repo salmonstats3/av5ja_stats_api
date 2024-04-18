bd1d4fd456f6510edc0bc8cc3817f0328838fb5c
tkgstrator <nasawake.am@gmail.com>
2024-04-19 06:25:47 +0900

    Add Big Run records (#22)
    * feat(record): add big run record
    
    * fix: remove console.log
    
    * feat: add time
    
    * fix: fix yaml

--- src/dto/av5ja/coop_record.dto.ts
+++ src/dto/av5ja/coop_record.dto.ts
@@ -1,6 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { Expose, Transform, Type } from 'class-transformer'
-import { IsEnum, IsOptional, ValidateNested } from 'class-validator'
+import { Expose, Transform, Type, plainToInstance } from 'class-transformer'
+import { IsEnum, IsInt, IsOptional, ValidateNested } from 'class-validator'
+import dayjs from 'dayjs'
 
 import { CoopEnemyInfoId } from '@/enum/coop_enemy'
 import { CoopGradeId } from '@/enum/coop_grade'
@@ -11,46 +12,6 @@ export namespace CoopRecordQuery {
     @ApiProperty({ required: true, type: 'integer' })
     @Expose({ name: 'coopEnemyId' })
     readonly id: number
-  }
-
-  class StageRecord {
-    @ApiProperty({ required: true, type: 'integer' })
-    @Expose({ name: 'coopStageId' })
-    readonly id: number
-  }
-
-  export class Node {
-    // readonly coopStage: Enemy
-    readonly endTime: Date
-    // readonly highestGrade: Grade
-    readonly highestGradePoint: number
-    readonly highestJobScore: number
-    readonly rankPercentile: number | null
-    readonly startTime: Date
-    readonly trophy: string
-  }
-
-  class Edge {
-    readonly node: Node
-  }
-
-  class Records {
-    readonly edges: Edge[]
-  }
-
-  class DefeatRecord {
-    @ApiProperty({ required: true, type: EnemyRecord })
-    @Expose()
-    @Type(() => EnemyRecord)
-    readonly enemy: EnemyRecord
-
-    @ApiProperty({ required: true, type: 'integer' })
-    @Expose({ name: 'defeatCount' })
-    readonly count: number
-  }
-
-  class BigRunRecord {
-    readonly records: Records
   }
 
   class CoopGrade {
@@ -72,7 +33,32 @@ export namespace CoopRecordQuery {
     readonly id: CoopGradeId | null
   }
 
+  class StageRecord {
+    @ApiProperty({ required: true, type: 'integer' })
+    @Expose({ name: 'coopStageId' })
+    @Transform(({ obj, value }) => {
+      if (value !== undefined) {
+        return value
+      }
+      const regexp = /-([0-9-]*)/
+      const match = regexp.exec(atob(obj.id))
+      return match === null ? CoopStageId.Dummy : parseInt(match[1], 10)
+    })
+    @IsEnum(CoopStageId)
+    readonly id: CoopStageId
+  }
+
   class StageHighestRecord {
+    @ApiProperty({ required: true, type: Date })
+    @Expose()
+    @Transform(({ value }) => (value == null ? null : dayjs(value).utc().toISOString()))
+    readonly startTime: Date
+
+    @ApiProperty({ required: true, type: Date })
+    @Expose()
+    @Transform(({ value }) => (value == null ? null : dayjs(value).utc().toISOString()))
+    readonly endTime: Date | null
+
     @ApiProperty({ required: true, type: StageRecord })
     @Expose()
     @Type(() => StageRecord)
@@ -81,32 +67,91 @@ export namespace CoopRecordQuery {
     @ApiProperty({ required: true, type: CoopGrade })
     @Expose()
     @Type(() => CoopGrade)
+    @Transform(({ obj }) =>
+      plainToInstance(CoopGrade, obj.grade ?? obj.highestGrade, { excludeExtraneousValues: true }),
+    )
     readonly grade: CoopGrade
 
     @ApiProperty({ required: true, type: 'integer' })
     @Expose()
-    readonly gradePoint: number
+    @Transform(({ obj }) => obj.gradePoint ?? obj.highestGradePoint)
+    @IsInt()
+    @IsOptional()
+    readonly gradePoint: number | null
+
+    @ApiProperty({ required: true, type: 'string' })
+    @Expose()
+    readonly trophy: string
+
+    @ApiProperty({ name: 'highestJobScore', nullable: true, required: false, type: 'integer' })
+    @Transform(({ value }) => (value === null ? null : value))
+    @Expose({ name: 'highestJobScore' })
+    readonly goldenIkuraNum: number | null
+
+    @ApiProperty({ name: 'rankPercentile', nullable: true, required: false, type: 'integer' })
+    @Expose({ name: 'rankPercentile' })
+    readonly rank: number | null
+  }
+
+  class Edge {
+    @ApiProperty({ required: true, type: StageHighestRecord })
+    @Expose()
+    @Type(() => StageHighestRecord)
+    @ValidateNested()
+    readonly node: StageHighestRecord
+  }
+
+  class Records {
+    @ApiProperty({ isArray: true, required: true, type: Edge })
+    @Expose()
+    @Type(() => Edge)
+    @ValidateNested({ each: true })
+    readonly edges: Edge[]
+  }
+
+  class DefeatRecord {
+    @ApiProperty({ required: true, type: EnemyRecord })
+    @Expose()
+    @Type(() => EnemyRecord)
+    @ValidateNested()
+    readonly enemy: EnemyRecord
+
+    @ApiProperty({ required: true, type: 'integer' })
+    @Expose({ name: 'defeatCount' })
+    readonly count: number
+  }
+
+  class BigRunRecord {
+    @ApiProperty({ required: true, type: Records })
+    @Expose()
+    @Type(() => Records)
+    @ValidateNested()
+    readonly records: Records
   }
 
   export class CoopRecordGroup {
     @ApiProperty({ required: true, type: BigRunRecord })
     @Type(() => BigRunRecord)
     @Expose()
+    @ValidateNested()
     readonly bigRunRecord: BigRunRecord
 
     @ApiProperty({ isArray: true, required: true, type: DefeatRecord })
     @Type(() => DefeatRecord)
     @Expose()
+    @ValidateNested({ each: true })
     readonly defeatBossRecords: DefeatRecord[]
 
     @ApiProperty({ isArray: true, required: true, type: DefeatRecord })
     @Type(() => DefeatRecord)
     @Expose()
+    @ValidateNested({ each: true })
     readonly defeatEnemyRecords: DefeatRecord[]
 
     @ApiProperty({ isArray: true, required: true, type: StageHighestRecord })
     @Type(() => StageHighestRecord)
     @Expose()
+    @ValidateNested({ each: true })
     readonly stageHighestRecords: StageHighestRecord[]
     // readonly teamContestRecord: null
   }
@@ -115,7 +160,7 @@ export namespace CoopRecordQuery {
     @ApiProperty({ required: true, type: CoopRecordGroup })
     @Expose()
     @Type(() => CoopRecordGroup)
-    @ValidateNested({ each: true })
+    @ValidateNested()
     readonly coopRecord: CoopRecordGroup
   }
 
@@ -123,8 +168,18 @@ export namespace CoopRecordQuery {
     @ApiProperty({ required: true, type: CoopRecordDataClass })
     @Expose()
     @Type(() => CoopRecordDataClass)
-    @ValidateNested({ each: true })
+    @ValidateNested()
     readonly data: CoopRecordDataClass
+
+    get stageHighestRecords(): StageHighestRecord[] {
+      return this.data.coopRecord.stageHighestRecords.concat(
+        this.data.coopRecord.bigRunRecord.records.edges.map((edge) => edge.node),
+      )
+    }
+
+    get enemyRecords(): DefeatRecord[] {
+      return this.data.coopRecord.defeatEnemyRecords.concat(this.data.coopRecord.defeatBossRecords)
+    }
   }
 
   class CoopEnemyRecord {
@@ -143,21 +198,21 @@ export namespace CoopRecordQuery {
 
   export class RecordResponse {
     constructor(record: RecordRequest) {
-      this.stageRecords = record.data.coopRecord.stageHighestRecords.map(({ coopStage, grade, gradePoint }) => ({
-        endTime: null,
-        goldenIkuraNum: 0,
-        grade: grade.id,
-        gradePoint,
-        stageId: coopStage.id,
-        startTime: null,
+      this.stageRecords = record.stageHighestRecords.map((record) => ({
+        endTime: record.endTime || null,
+        goldenIkuraNum: record.goldenIkuraNum || null,
+        grade: record.grade.id,
+        gradePoint: record.gradePoint,
+        rank: record.rank || null,
+        stageId: record.coopStage.id,
+        startTime: record.startTime || null,
+        trophy: record.trophy || null,
       }))
 
-      this.enemyRecords = record.data.coopRecord.defeatEnemyRecords
-        .concat(record.data.coopRecord.defeatBossRecords)
-        .map(({ enemy, count }) => ({
-          count: count,
-          enemyId: enemy.id,
-        }))
+      this.enemyRecords = record.enemyRecords.map(({ enemy, count }) => ({
+        count: count,
+        enemyId: enemy.id,
+      }))
     }
 
     readonly stageRecords: CoopStageRecord[]
