a36e1e85f6844f982bc4ad65f232cb4e533c9a7e
tkgstrator <nasawake.am@gmail.com>
2024-04-18 13:28:53 +0000

    fix: remove console.log

--- src/schedules/schedules.service.ts
+++ src/schedules/schedules.service.ts
@@ -52,7 +52,6 @@ export class SchedulesService {
           getDocs(query(collection(this.firestore, setting), orderBy('startTime', 'desc'))),
         ),
       )
-      console.log(request)
       const schedules: CoopSchedule[] = documents
         .flatMap((document) =>
           document.docs.map((doc) =>
